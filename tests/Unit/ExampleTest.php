77e23221e1e0095793f17ea4d46f8ce4b6b50ef2
CodingLama96 <lmanaser96@gmail.com>
2024-01-06 00:29:32 +0100

    Fixed database reset issue after testing

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
